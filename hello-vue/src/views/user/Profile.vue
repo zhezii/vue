683c95000bcb091e3505bd806b1da383a1f45264
zhezii <1115384864@qq.com>
2019-06-26 23:33:45 +0800

    配置嵌套路由

---
+++ hello-vue/src/views/user/Profile.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      个人信息ID = {{ id }}
+    </div>
+</template>
+
+<script>
+    export default {
+      props: ['id'],
+      name: "UserProfile"
+    }
+</script>
+
+<style scoped>
+
+</style>
